c0c008d9c607ad4015aa4be8bd41b3098366276a
NunoLima10 <84800684+NunoLima10@users.noreply.github.com>
2022-10-02 16:50:22 -0100

    fix: add values to data base

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,20 @@
-use std::collections::HashMap;
+use std::{collections::HashMap};
 fn main() {
     let mut arguments = std::env::args().skip(1);
 
     let key = arguments.next().unwrap();
     let value = arguments.next().unwrap();
 
-    let data = format!("{}\t{}\n",key,value);
-    std::fs::write("qs.db", data).unwrap();
+    let mut data_base =  DataBase::new().expect("data base error");
+    data_base.add(key.to_uppercase(),value.clone());
+    data_base.add(key,value);
+    data_base.flush().unwrap();
 }
 struct  DataBase{
     map: HashMap<String,String>,
 }
 impl DataBase {
     fn new() -> Result<DataBase, std::io::Error> {
-        // let data =  match std::fs::read_to_string("qs.db") {
-        //     Ok(d) => d,
-        //     Err(error)=>{
-        //         return  Err(error);
-        //     }
-        // };
-
         let data = std::fs::read_to_string("qs.db")?;
         let mut  map = HashMap::new();
 
@@ -30,8 +25,22 @@ impl DataBase {
             let value = pair.next().expect("No value");
             
             map.insert(key.to_owned(), value.to_owned());
+        }  
+
+        Ok(DataBase{map})
+    }
+    fn add(&mut self, key: String, value: String){
+        self.map.insert(key, value);
+    }
+    fn flush(self) -> std::io::Result<()>{
+        let mut  data = String::new();
+        for (key, value) in &self.map{
+            data.push_str(key);
+            data.push('\t');
+            data.push_str(value);
+            data.push('\n');
         }
 
-        Ok(DataBase{map: map})
+        std::fs::write("qs.db", data)
     }
 }
